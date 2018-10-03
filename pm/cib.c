875e7ae15da21d8617cb0706caa1dfebcf6edbec
Samuel Larsson <larsson.samuel@live.se>
2018-10-03 17:16:37 +0200

    Create CIBNode method skeletons

--- pm/cib.c
+++ pm/cib.c
@@ -6,6 +6,8 @@
 #include "jansson.h"
 #include <sys/queue.h>
 
+/*----- START CIBNode -----*/
+
 typedef struct CIBNode_s CIBNode_t;
 
 typedef struct linked_CIBNode {
@@ -29,21 +31,45 @@ typedef struct CIBNode_s {
     json_t *properties;
     json_t *match;
     struct list linked_nodes;
+
+    bool (*match_entry)(CIBNode_t *, json_t);
+    bool (*update_links_from_match)(CIBNode_t *);
+    //TODO
 } CIBNode_t;
 
-CIBNode_t *CIBNode_constructor(){
+bool match_entry(CIBNode_t *self, json_t entry){
+    //TODO
+    return false;
+}
+
+bool update_links_from_match(CIBNode_t *self){
+    //TODO
+    return false;
+}
+
+CIBNode_t *CIBNode_init(){
     CIBNode_t *node = malloc(sizeof(CIBNode_t));
+    node->uid = "";
     node->root = false;
     node->link = false;
     node->priority = 0;
     node->expire = time(NULL);
+    node->filename = "";
+    node->description = "";
+    node->properties = malloc(sizeof(json_array));
+    node->match = malloc(sizeof(json_array));
     LIST_INIT(&node->linked_nodes.self);
+
+    node->match_entry = match_entry;
+    node->update_links_from_match = update_links_from_match;
     return node;
 }
 
+/*----- END CIBNode -----*/
+
 int main(int argc, char **argv) {
     // Testing the list with linked nodes
-    CIBNode_t *node = CIBNode_constructor();
+    CIBNode_t *node = CIBNode_init();
 
     linked_CIBNode *linked_node = &(linked_CIBNode) {
         .inner_node = malloc(sizeof(CIBNode_t))
