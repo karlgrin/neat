ded1baa41f31cf5d8b089fdcfa1c136395476123
Samuel Larsson <larsson.samuel@live.se>
2018-10-16 19:43:47 +0200

    Change executable name

--- pm/run.sh
+++ pm/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 clear
-gcc pmhelper.c property.c multi_property.c node.c pib.c -Wall -o test.out -ljansson
-./test.out
+gcc pmhelper.c property.c multi_property.c node.c pib.c cib.c neat_pm.c -Wall -ggdb -o neat_pm -ljansson
+./test
